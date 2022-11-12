8376ff1c59baf649ea7309da28da27570d21d2ba
mosecanon <58547280+mosecanon@users.noreply.github.com>
2022-11-12 09:51:38 +0000

    Update load.php

--- load.php
+++ load.php
@@ -1,9 +1,11 @@
 <?php
 
-$connection = mysqli_connect('localhost', 'root', '', 'pace');
+$connection = mysqli_connect('localhost', 'root', '', 'your_database_name');
 
 if($connection) {
     echo 'connected';
 } else {
     echo 'Failed';
 }
+
+//You can load your data from any file or database...the concept is the same
