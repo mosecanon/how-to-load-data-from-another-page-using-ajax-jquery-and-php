791e83c4ecfafad69f3f0fecb2d1718663225c3c
mosecanon <58547280+mosecanon@users.noreply.github.com>
2022-11-12 09:49:03 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>How to load data from another page using Ajax</title>
+</head>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<body>
+    
+<div class="load_data_container">
+
+</div>
+
+</body>
+<script>
+    $(document).ready(function() {
+        $('.load_data_container').load('load.php')
+    })
+</script>
+
+</html>
